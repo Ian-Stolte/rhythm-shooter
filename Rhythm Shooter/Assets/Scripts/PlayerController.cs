158f2ac79697828f4f017a038bdbc34239c2d75a
Ian-Stolte <ianstolte@gmail.com>
2024-07-25 11:57:20 -0700

    Bullet indicator and spawning bullets

--- Rhythm Shooter/Assets/Scripts/PlayerController.cs
+++ Rhythm Shooter/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private GameObject directionIndicator;
+    [SerializeField] private float mouseAngle;
+    private Vector3 bulletDir;
+
+    [SerializeField] private GameObject bulletPrefab;
 
     void Start()
     {
-        directionIndicator = GameObject.Find("Direction Indicator");        
+        directionIndicator = GameObject.Find("Direction Indicator");
     }
 
     void Update()
     {
-        directionIndicator.transform.RotateAround(transform.position, Vector3.Up, 90/*get angle from mouse*/);
+        mouseAngle = GetMouseRot();
+        directionIndicator.transform.RotateAround(transform.position, new Vector3(0, 0, 1), mouseAngle - directionIndicator.transform.rotation.eulerAngles.z);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireBullet();
+        }
+    }
+
+    private float GetMouseRot()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Rect canvasRect = GameObject.Find("Canvas").GetComponent<RectTransform>().rect;
+        Vector3 canvasScale = GameObject.Find("Canvas").GetComponent<RectTransform>().localScale;
+        float mouseXChange = mousePos.x - 0.5f*canvasRect.width*canvasScale.x;
+        float mouseYChange = mousePos.y - 0.5f*canvasRect.height*canvasScale.y;
+        bulletDir = new Vector3(mouseXChange, mouseYChange, 0);
+        bulletDir = Vector3.Normalize(bulletDir);
+        return(Mathf.Atan2(mouseYChange, mouseXChange) * Mathf.Rad2Deg - 90);
+    }
+
+    private void FireBullet()
+    {
+        Instantiate(bulletPrefab, transform.position + bulletDir, Quaternion.identity, GameObject.Find("Bullets").transform);
     }
 }
